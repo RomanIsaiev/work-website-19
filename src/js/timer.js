bc183793eaf02e966be55252ab2fd57cf8d5654e
Roman Isaiev <roma.isaev.2000@gmail.com>
2024-01-05 23:15:46 +0200

    webstite completed

--- src/js/timer.js
+++ src/js/timer.js
@@ -1,6 +1,6 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
   // конечная дата, например 1 июля 2021
-  const deadline = new Date(2024, 2, 1, 8, 0, 0);
+  const deadline = new Date(2024, 0, 8, 22, 0, 0);
   // id таймера
   let timerId = null;
   // склонение числительных
@@ -21,73 +21,73 @@ document.addEventListener('DOMContentLoaded', function () {
     const hours = diff > 0 ? Math.floor(diff / 1000 / 60 / 60) % 24 : 0;
     const minutes = diff > 0 ? Math.floor(diff / 1000 / 60) % 60 : 0;
     const seconds = diff > 0 ? Math.floor(diff / 1000) % 60 : 0;
-    $days.textContent = days < 10 ? '0' + days : days;
-    $hours.textContent = hours < 10 ? '0' + hours : hours;
-    $minutes.textContent = minutes < 10 ? '0' + minutes : minutes;
-    $seconds.textContent = seconds < 10 ? '0' + seconds : seconds;
-    $days.dataset.title = declensionNum(days, ['день', 'дні', 'днів']);
-    $hours.dataset.title = declensionNum(hours, ['година', 'години', 'годин']);
+    $days.textContent = days < 10 ? "0" + days : days;
+    $hours.textContent = hours < 10 ? "0" + hours : hours;
+    $minutes.textContent = minutes < 10 ? "0" + minutes : minutes;
+    $seconds.textContent = seconds < 10 ? "0" + seconds : seconds;
+    $days.dataset.title = declensionNum(days, ["день", "дня", "дней"]);
+    $hours.dataset.title = declensionNum(hours, ["час", "часа", "часов"]);
     $minutes.dataset.title = declensionNum(minutes, [
-      'хвилина',
-      'хвилини',
-      'хвилин',
+      "минута",
+      "минуты",
+      "минут",
     ]);
     $seconds.dataset.title = declensionNum(seconds, [
-      'секунда',
-      'секунди',
-      'секунд',
+      "секунд",
+      "секунды",
+      "секунд",
     ]);
   }
 
   // получаем элементы, содержащие компоненты даты
-  const $days = document.querySelector('.timer__days');
-  const $hours = document.querySelector('.timer__hours');
-  const $minutes = document.querySelector('.timer__minutes');
-  const $seconds = document.querySelector('.timer__seconds');
+  const $days = document.querySelector(".timer__days");
+  const $hours = document.querySelector(".timer__hours");
+  const $minutes = document.querySelector(".timer__minutes");
+  const $seconds = document.querySelector(".timer__seconds");
   // вызываем функцию countdownTimer
   countdownTimer();
 
-  function countdownTimerTwo() {
-    const diff = deadline - new Date();
-    if (diff <= 0) {
-      clearInterval(timerId);
-    }
-    const daysTwo = diff > 0 ? Math.floor(diff / 1000 / 60 / 60 / 24) : 0;
-    const hoursTwo = diff > 0 ? Math.floor(diff / 1000 / 60 / 60) % 24 : 0;
-    const minutesTwo = diff > 0 ? Math.floor(diff / 1000 / 60) % 60 : 0;
-    const secondsTwo = diff > 0 ? Math.floor(diff / 1000) % 60 : 0;
-    $daysTwo.textContent = daysTwo < 10 ? '0' + daysTwo : daysTwo;
-    $hoursTwo.textContent = hoursTwo < 10 ? '0' + hoursTwo : hoursTwo;
-    $minutesTwo.textContent = minutesTwo < 10 ? '0' + minutesTwo : minutesTwo;
-    $secondsTwo.textContent = secondsTwo < 10 ? '0' + secondsTwo : secondsTwo;
-    $daysTwo.dataset.title = declensionNum(daysTwo, ['день', 'дні', 'днів']);
-    $hoursTwo.dataset.title = declensionNum(hoursTwo, [
-      'година',
-      'години',
-      'годин',
-    ]);
-    $minutesTwo.dataset.title = declensionNum(minutesTwo, [
-      'хвилина',
-      'хвилини',
-      'хвилин',
-    ]);
-    $secondsTwo.dataset.title = declensionNum(secondsTwo, [
-      'секунда',
-      'секунди',
-      'секунд',
-    ]);
-  }
+  // function countdownTimerTwo() {
+  //   const diff = deadline - new Date();
+  //   if (diff <= 0) {
+  //     clearInterval(timerId);
+  //   }
+  //   const daysTwo = diff > 0 ? Math.floor(diff / 1000 / 60 / 60 / 24) : 0;
+  //   const hoursTwo = diff > 0 ? Math.floor(diff / 1000 / 60 / 60) % 24 : 0;
+  //   const minutesTwo = diff > 0 ? Math.floor(diff / 1000 / 60) % 60 : 0;
+  //   const secondsTwo = diff > 0 ? Math.floor(diff / 1000) % 60 : 0;
+  //   $daysTwo.textContent = daysTwo < 10 ? "0" + daysTwo : daysTwo;
+  //   $hoursTwo.textContent = hoursTwo < 10 ? "0" + hoursTwo : hoursTwo;
+  //   $minutesTwo.textContent = minutesTwo < 10 ? "0" + minutesTwo : minutesTwo;
+  //   $secondsTwo.textContent = secondsTwo < 10 ? "0" + secondsTwo : secondsTwo;
+  //   $daysTwo.dataset.title = declensionNum(daysTwo, ["день", "дні", "днів"]);
+  //   $hoursTwo.dataset.title = declensionNum(hoursTwo, [
+  //     "година",
+  //     "години",
+  //     "годин",
+  //   ]);
+  //   $minutesTwo.dataset.title = declensionNum(minutesTwo, [
+  //     "хвилина",
+  //     "хвилини",
+  //     "хвилин",
+  //   ]);
+  //   $secondsTwo.dataset.title = declensionNum(secondsTwo, [
+  //     "секунда",
+  //     "секунди",
+  //     "секунд",
+  //   ]);
+  // }
 
-  const $daysTwo = document.querySelector('.timer__daysTwo');
-  const $hoursTwo = document.querySelector('.timer__hoursTwo');
-  const $minutesTwo = document.querySelector('.timer__minutesTwo');
-  const $secondsTwo = document.querySelector('.timer__secondsTwo');
+  // const $daysTwo = document.querySelector(".timer__daysTwo");
+  // const $hoursTwo = document.querySelector(".timer__hoursTwo");
+  // const $minutesTwo = document.querySelector(".timer__minutesTwo");
+  // const $secondsTwo = document.querySelector(".timer__secondsTwo");
 
-  countdownTimerTwo();
+  // countdownTimerTwo();
 
   // вызываем функцию countdownTimer каждую секунду
   timerId = setInterval(countdownTimer, 1000);
-  timerId = setInterval(countdownTimerTwo, 1000);
+  // timerId = setInterval(countdownTimerTwo, 1000);
 });
 
 // $days.dataset.title = declensionNum(days, ['день', 'дні', 'днів']);
